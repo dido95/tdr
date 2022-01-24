c0c64e740bdd700d85c3a0cea08b7ee1d2d7dbde
dido95 <76779936+dido95@users.noreply.github.com>
2022-01-25 07:41:14 +0800

    Initial commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,272 @@
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import {
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  Image,
+  ViewComponent,
+  StyleSheet,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import useAuth from "../hooks/useAuth";
+import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "tailwind-rn";
+import { Ionicons, Entypo, AntDesign } from "@expo/vector-icons";
+import Swiper from "react-native-deck-swiper";
+import * as ImagePicker from 'expo-image-picker';
+import {
+  collection,
+  doc,
+  DocumentSnapshot,
+  getDoc,
+  getDocs,
+  onSnapshot,
+  query,
+  serverTimestamp,
+  setDoc,
+  where,
+} from "@firebase/firestore";
+import { db } from "../firebase";
+import generateId from "../lib/generateId";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+  const { user, logout } = useAuth();
+  const [profiles, setProfiles] = useState([]);
+  const swipeRef = useRef(null);
+
+  useLayoutEffect(
+    () =>
+      onSnapshot(doc(db, "users", user.uid), (snapshot) => {
+        if (!snapshot.exists()) {
+          navigation.navigate("Modal");
+        }
+      }),
+    []
+  );
+  useEffect(() => {
+    let unsub;
+
+    const fetchCards = async () => {
+      const passes = await getDocs(
+        collection(db, "users", user.uid, "passes")
+      ).then((snapshot) => snapshot.docs.map((doc) => doc.id));
+
+      const swipes = await getDocs(
+        collection(db, "users", user.uid, "swipes")
+      ).then((snapshot) => snapshot.docs.map((doc) => doc.id));
+
+      const passedUserIds = passes.length > 0 ? passes : ["test"];
+      const swipedUserIds = swipes.length > 0 ? passes : ["test"];
+
+      unsub = onSnapshot(
+        query(
+          collection(db, "users"),
+          where("id", "not-in", [...passedUserIds, ...swipedUserIds])
+        ),
+        (snapshot) => {
+          setProfiles(
+            snapshot.docs
+              .filter((doc) => doc.id !== user.uid)
+              .map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+              }))
+          );
+        }
+      );
+    };
+    fetchCards();
+    return unsub;
+  }, [db]);
+
+  const swipeLeft = (cardIndex) => {
+    if (!profiles[cardIndex]) return;
+    const userSwiped = profiles[cardIndex];
+    setDoc(doc(db, "users", user.uid, "passes", userSwiped.id), userSwiped);
+  };
+  const swipeRight = async (cardIndex) => {
+    if (!profiles[cardIndex]) return;
+
+    const userSwiped = profiles[cardIndex];
+    const loggedInProfile = await (
+      await getDoc(doc(db, "users", user.uid))
+    ).data();
+
+    getDoc(doc(db, "users", userSwiped.id, "swipes", user.uid)).then(
+      (documentSnapshot) => {
+        if (documentSnapshot.exists()) {
+          setDoc(
+            doc(db, "users", user.uid, "swipes", userSwiped.id),
+            userSwiped
+          );
+          setDoc(doc(db, "matches", generateId(user.uid, userSwiped.id)), {
+            users: {
+              [user.uid]: loggedInProfile,
+              [userSwiped.id]: userSwiped,
+            },
+            usersMatched: [user.uid, userSwiped.id],
+            timeStamp: serverTimestamp(),
+          });
+          navigation.navigate("Match", {
+            loggedInProfile,
+            userSwiped,
+          });
+        } else {
+          setDoc(
+            doc(db, "users", user.uid, "swipes", userSwiped.id),
+            userSwiped
+          );
+        }
+      }
+    );
+  };
+  return (
+    <SafeAreaView style={tw("flex-1")}>
+      {/* header */}
+      <View style={tw("flex-row items-center justify-between px-6 pt-3")}>
+        <TouchableOpacity onPress={logout}>
+          <Image
+            style={tw("h-10 w-10 rounded-full")}
+            source={{ uri: user.photoURL }}
+          ></Image>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate("Modal")}>
+          <Ionicons name="book" size={38} color="#191970" />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate("Chat")}>
+          <Ionicons name="chatbubble-sharp" size={38} color="#191970" />
+        </TouchableOpacity>
+      </View>
+      {/* end header */}
+      {/* cards rendering */}
+      <View style={tw("flex-1 -mt-6 ")}>
+        <Swiper
+          ref={swipeRef}
+          containerStyle={{ backgroundColor: "transparent" }}
+          cards={profiles}
+          stackSize={5}
+          cardIndex={0}
+          verticalSwipe={false}
+          animateCardOpacity
+          onSwipedLeft={(cardIndex) => {
+            swipeLeft(cardIndex);
+          }}
+          onSwipedRight={(cardIndex) => {
+            swipeRight(cardIndex);
+          }}
+          backgroundColor={"#4FD0E9"}
+          overlayLabels={{
+            left: {
+              title: "NOPE",
+              style: {
+                label: {
+                  textAlign: "right",
+                  color: "red",
+                },
+              },
+            },
+            right: {
+              title: "LIKE",
+              style: {
+                label: {
+                  textAlign: "left",
+                  color: "green",
+                },
+              },
+            },
+          }}
+          renderCard={(card) =>
+            card ? (
+              <View
+                key={card.id}
+                style={tw("relative bg-white h-3/4 rounded-xl")}
+              >
+                <Image
+                  style={tw("absolute top-0 h-full w-full rounded-xl")}
+                  source={{ uri: card.photoURL }}
+                />
+
+                <View
+                  style={[
+                    tw(
+                      "absolute bottom-0 bg-white w-full h-32 justify-between flex-row px-6 rounded-b-lg"
+                    ),
+                    styles.cardShadow,
+                  ]}
+                >
+                  <View>
+                    <Text style={tw("text-xl font-bold")}>
+                      {card.displayName}
+                    </Text>
+                    <Text style={tw("text-lg font-bold")}>{card.career}</Text>
+                    <Text>{card.bio}</Text>
+                    <Text>{card.isChecked === true ? "Doy tutorias" : "No disponiple para tutorias"}</Text>
+                  </View>
+                  <Text style={tw("text-2xl font-bold text-red-400")}>{card.age}</Text>
+                </View>
+              </View>
+            ) : (
+              <View
+                style={[
+                  tw(
+                    "relative bg-white h-3/4 rounded-xl justify-center items-center"
+                  ),
+                  styles.cardShadow,
+                ]}
+              >
+                <Text style={tw("font-bold pb-5")}>
+                  Ese fue el ultimo perfil!!
+                </Text>
+                <Image
+                  source={{ uri: "https://links.papareact.com/6gb" }}
+                  style={tw("h-20 w-full")}
+                  height={100}
+                  width={100}
+                />
+              </View>
+            )
+          }
+        />
+      </View>
+      {/* end of the card view */}
+
+      <View style={tw("flex flex-row justify-evenly")}>
+        <TouchableOpacity
+          style={tw(
+            "items-center justify-center rounded-full w-16 h-16 bg-red-200"
+          )}
+          onPress={() => swipeRef.current.swipeLeft()}
+        >
+          <Entypo name="cross" size={34} color="red" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={tw(
+            "items-center justify-center rounded-full w-16 h-16 bg-green-200"
+          )}
+          onPress={() => swipeRef.current.swipeRight()}
+        >
+          <AntDesign name="heart" size={34} color="green" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  cardShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 0.89,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.2,
+    elevation: 2,
+  },
+});
